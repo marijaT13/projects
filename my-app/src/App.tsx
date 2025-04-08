7fd172f51d639d37977c6cd96673cd5f6ecfdbdb
marijaT13 <tashevska254@gmail.com>
2025-04-08 13:00:23 +0200

    Squashed 'timer/' content from commit 9668057
    git-subtree-dir: timer
    git-subtree-split: 9668057e416389be0c8c04102f4eb3d8f58bdcd8

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Timer from "./Timer";
+import './App.css';
+import './main.tsx';
+function App() {
+  return (
+    <div className='App'>
+      <Timer />
+    </div>
+  );
+}
+
+export default App
